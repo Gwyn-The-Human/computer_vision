10ba338edd4f16af3c5ee8424deeaf0784b85ce5
Gwyn <gwyn_g@live.com>
2022-08-21 18:29:42 +0100

    updates readme; adds play_game function to manual_rps

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 #Trained a model using https://teachablemachine.withgoogle.com/ to recognise 4 classes (rock, paper, scissors and none) trained on about 3000 images for each class
 #The model is saved as keras_model.h5 in this repo. 
 #I'll be using the model to build a rock-paper-scissors game in which the computer will use compture vision to play against me! But I will win anyway! 
+
+
+#SECOND MILESTONE
+
+
+#wrote a little rock-paper-scissors game; used the random library to randomise computer choice. 
+#tried to do something more itneresting with the logic for get_winner than just hard coding "if user == rock and computer #== paper" etc. etc.; instead tried having a list of possible choices, and arranging them so that each item loses to the #left and wins to the right. From this list we can describe how the choices interact by comparing the indexes of the #players' choices in that list. 
--- manual_rps.py
+++ manual_rps.py
@@ -1,9 +1,10 @@
 import random
-cancel
-asdsa
-asdasd
-asdasd
-e(choices):
+
+
+
+
+
+def get_computer_choice(choices):
     
     return choices [random.randint (0,2)]
 
@@ -14,17 +15,17 @@ def get_user_choice ():
 
 
 def get_winner ():
-    choices = ["rock", "paper", "scissors"]
+    choices = ["rock", "paper", "scissors"] #in this list index 0 beats 1 beats 2 beats 0
     user = get_user_choice ()
     computer = get_computer_choice (choices)
     print (f"Computer chose {computer}!")
     if user == computer:
         print ("Draw! Don't give up!")
         get_winner ()
-    if choices.index (user) +1 == choices.index (computer) or choices.index(user)-2 == choices.index (computer):
+    if choices.index (user) +1 == choices.index (computer) or choices.index(user)-2 == choices.index (computer): #describes the relationship in comment on line 18
         return "User wins!"
     else:
-        return "Computer Wins!"
+        return "Computer Wins!" #should this be a print line?
 
-
-print(get_winner())
+def play_game():
+    return get_winner()
